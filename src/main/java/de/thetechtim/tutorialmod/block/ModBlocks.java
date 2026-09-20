e3b736fc76d030e4a735bdaea2c3de8f5bac2fe1
thetechtim <tim.ebenhardt@outlook.de>
2026-09-20 22:38:57 +0200

    added bismuth block and ore

---
+++ src/main/java/de/thetechtim/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,50 @@
+package de.thetechtim.tutorialmod.block;
+
+import de.thetechtim.tutorialmod.TutorialMod;
+import de.thetechtim.tutorialmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(TutorialMod.MODID);
+
+
+    public static final DeferredBlock<Block> BISMUTH_BLOCK = registerBlock("bismuth_block",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)
+            ));
+
+    public static final DeferredBlock<Block> BISMUTH_ORE = registerBlock("bismuth_ore",
+            () -> new DropExperienceBlock(UniformInt.of(2, 4),
+                    BlockBehaviour.Properties.of()
+                            .strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)
+            ));
+
+
+
+    // Register for Item
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBockItem(name, toReturn);
+        return toReturn;
+    }
+    // Register for Item here
+    public static <T extends Block> void registerBockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+            BLOCKS.register(eventBus);
+    }
+
+}
