369a65db534644c83cfb557690446f390b692a39
thetechtim <tim.ebenhardt@outlook.de>
2026-09-20 22:38:06 +0200

    added bismuth and raw_bismuth

---
+++ src/main/java/de/thetechtim/tutorialmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package de.thetechtim.tutorialmod.item;
+
+import de.thetechtim.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TutorialMod.MODID);
+
+    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
